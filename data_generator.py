ce44e2ec83a233edd8d716b089cf891608f967ae
efekaanefe <efekaanefe1905@gmail.com>
2024-02-01 16:37:38 +0300

    data generator regression/classification

--- data_generator.py
+++ data_generator.py
@@ -1,4 +1,4 @@
-from sklearn.datasets import make_classification
+from sklearn.datasets import make_classification, make_regression
 
 
 class DataGenerator:
@@ -7,16 +7,23 @@ class DataGenerator:
         self.num_features = num_features
         self.num_classes = num_classes
 
-    def get_data(self):
-        X, y = make_classification(n_samples=self.num_samples, 
-                                   n_features=self.num_features, 
-                                   n_classes=self.num_classes, 
-                                   n_clusters_per_class=1, 
-                                   n_redundant=0, 
-                                   n_informative=2, 
-                                   random_state=42)
+    def get_data(self, type = "classification"):
+        if type == "classification":
+            X, y = make_classification(n_samples=self.num_samples, 
+                                       n_features=self.num_features, 
+                                       n_classes=self.num_classes, 
+                                       n_clusters_per_class=1, 
+                                       n_redundant=0, 
+                                       n_informative=2, 
+                                       random_state=42)
+        if type == "regression":
+            X, y = make_regression(n_samples=self.num_samples, 
+                                       n_features=self.num_features, 
+                                       noise = 10,
+                                       random_state=42)
+ 
         return X, y
-
+        
 
 if __name__ == "__main__":
     num_samples = 50,
