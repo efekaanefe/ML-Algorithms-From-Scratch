c9ed95c34543329081a81510158c2f4e8491172f
efekaanefe <efekaanefe1905@gmail.com>
2024-02-01 17:22:41 +0300

    plotter util

--- util.py
+++ util.py
@@ -1,5 +1,5 @@
 from sklearn.datasets import make_classification, make_regression
-
+import matplotlib.pyplot as plt
 
 class DataGenerator:
     def __init__(self, num_samples, num_features, num_classes=None):
@@ -25,9 +25,45 @@ class DataGenerator:
         return X, y
         
 
+class DataPlotter:
+    def __init__(self):
+        pass
+
+    def plot_data(self, X, y, type = "classification"):
+        if type == "classification":
+            # works for num_features = 2, and num_class < 3 
+            plt.figure(figsize=(8, 6))
+            colors = ['blue', 'red', 'green']
+            for i in range(3):
+                plt.scatter(X[y == i][:, 0], X[y == i][:, 1], color=colors[i], label=f'Class {i}')
+            plt.xlabel('Feature 1')
+            plt.ylabel('Feature 2')
+            plt.title('Classification Data')
+            plt.legend()
+            plt.grid(True)
+            plt.show()
+
+        if type == "regression":
+            plt.figure(figsize=(8, 6))
+            plt.plot(y, color='blue', label='Data points')
+            plt.xlabel('Index')
+            plt.ylabel('y')
+            plt.title('Regression Data')
+            plt.legend()
+            plt.grid(True)
+            plt.show()
+            
+
 if __name__ == "__main__":
+    ## hyper parameters
     num_samples = 50
     num_features = 3
     num_classes = 2
+    #type = "regression"
+    type = "classification"
+    
     gen = DataGenerator(num_samples, num_features, num_classes)
-    X, y = gen.get_data()
+    X, y = gen.get_data(type)
+    
+    plotter = DataPlotter()
+    plotter.plot_data(X,y, type = type)
